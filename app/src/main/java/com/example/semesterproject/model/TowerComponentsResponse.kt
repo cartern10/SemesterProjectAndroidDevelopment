2e1c7256aa1814f6f378c05c4986cb11821813be
cartern10 <cartern10@mymail.nku.edu>
2023-04-06 17:38:07 -0400

    deleted parts of TowerComponentsResponse

--- app/src/main/java/com/example/semesterproject/model/TowerComponentsResponse.kt
+++ app/src/main/java/com/example/semesterproject/model/TowerComponentsResponse.kt
@@ -1,22 +1,6 @@
 package com.example.semesterproject.model
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = false)
 data class TowerComponentsResponse(
-    val info: Info,
-    @field:Json(name = "results")
-    val towers: List<Tower>,
-)
-
-data class Info(
-    @Json(name = "count")
-    val count: Int,
-    @Json(name = "next")
-    val next: String,
-    @Json(name = "pages")
-    val pages: Int,
-    @Json(name = "prev")
-    val prev: Any?,
-)
